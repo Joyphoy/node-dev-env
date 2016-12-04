a5f710087fc42456e2c6acd631bab67fefe15c8a
Joonas Siltala <siltala.joonas@gmail.com>
2016-12-04 22:51:48 +0200

    Add webpack

--- scripts/server.js
+++ scripts/server.js
@@ -2,11 +2,20 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import dotenv from 'dotenv';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import config from '../webpack.config.dev';
 
 dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
+const compiler = webpack(config);
+
+app.use(webpackMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
